0ca477cedd2c9ef94140195fe38d76e4d4fd250f
Anandia Muhammad Yudhistira <anandiamy@gmail.com>
2019-08-23 15:55:51 +0700

    set model to config (#6)
    * set model to config
    
    * fixing typo

--- config/config.php
+++ config/config.php
@@ -5,5 +5,9 @@
 return [
     // Whether to auto load migrations or not.
     // If set to false, then you must publish the migration files first before running the migrate command
-    'migrations' => true
+    'migrations' => true,
+    'models'                => [
+        'role'       => \Laravolt\Acl\Models\Role::class,
+        'permission' => \Laravolt\Acl\Models\Permission::class
+    ],
 ];
